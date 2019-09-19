b0295057c05cd8d18257a97d059c8cff8d0e7652
skpanagiotis <panagiotis.skias@gmail.com>
2019-09-19 23:26:32 +0300

    Update Documentation and change the name of some variables

--- assignment-2018-1/present_bias.py
+++ assignment-2018-1/present_bias.py
@@ -1,18 +1,22 @@
 import argparse
 import copy
 
-# This method takes a file and makes a graph and save the distances and destination
+# This method takes a .txt file and makes a graph
 def filetograph (file):
     input_filename = file
-    #graph o grafos mas me key ton enan komvo kai values tous geitones komvous
+
+    #graph : the graph with KEY a node and as VALUE the neighbors of KEY node
     global graph
     graph = {}
-    #distance ena dictionary me key 2 komvous kai value h apostasi metaksi tous
+
+    #distance : a dictionary with KEY 2 nodes and as VALUE the distance between them
     global distance
     distance = {}
-    #destination ena dictionary me key enan komvo kai value tous komvous pou mporoume na pame apo to kombo key
+
+    #destination : a dictionary with KEY a node and as VALUE the nodes that we can go from KEY node
     global destination
     destination = {}
+    
     with open(input_filename) as graph_input:
         for line in graph_input:
             nodes = [str(x) for x in line.split()]
@@ -36,56 +40,66 @@ def filetograph (file):
             graph[nodes[0]].append(nodes[1])
             graph[nodes[1]].append(nodes[0])
 
-# This method takes the graph and find all the available paths
+# This method takes the graph and finds all the available paths
 def searchpath(start, end):
-    global diadromes
-    diadromes = [] #apothikevonte oi efiktes diadromes
+    global paths
+    paths = [] #Saving all available paths
     SVis = [] #The Stack as list which include the nodes that we have visited
     visited = {}
-    keysli = {} #xrisimopoiete gia tin apothikefsi ton klidion
-    diadromesvisited = {}
+    keysli = {} #we use this dictionary for saving keys
+    pathsvisited = {}
     keysli = list(graph.keys())
-    #dimiourgia dictionary gia to pies diadromes exoume kanei px ('s t' : False)
+
+    # Create a dictionary to mark which route we have done
+    # for example x node and y node
+    # if we have done xy path then ('x y' : True)
     for i in range (len(list(distance.keys()))):
-        diadromesvisited[list(distance.keys())[i]] = False
-    #dhmiourgia dicitonary gia to pious komvous episkeptomaste
+        pathsvisited[list(distance.keys())[i]] = False
+    
+    # Create a dictionary to mark which node we have visited 
     for i in range(len(graph)):
         visited [keysli[i]] = False
-    # enarksi periplanisis stous komvous
+    
+    # Start wandering to the graph
     visited[start] = True
     SVis.append(start)
     telos = False
-    boithvisited = visited
+    supvisited = visited
+
     while (telos == False):
         if (len(SVis) == 0):
             break
         a = SVis.pop(len(SVis) - 1)
         geitones = 0
+
         for y in list(destination.get(a)):
-            if (diadromesvisited.get(a + " " + y) == True): #metraei gitones
+            if (pathsvisited.get(a + " " + y) == True): #counts the neighbors
                 geitones = geitones + 1
-            if (geitones == len(destination.get(a))): #kanei olous tou geitones tou false an tous exoume episkefti
+
+            if (geitones == len(destination.get(a))): #makes all neighbors True if we have visited them
                 for w in list(destination.get(a)):
-                    boithvisited[w] = False
-                    diadromesvisited[a + " " + w] = False
+                    supvisited[w] = False
+                    pathsvisited[a + " " + w] = False
                     visited[w] = False
                 break
-            if (diadromesvisited.get(a + " " + y) == False):
+
+            if (pathsvisited.get(a + " " + y) == False):
                 visited[y] = True
-                boithvisited[y] = True
+                supvisited[y] = True
                 SVis.append(a)
                 SVis.append(y)
-                diadromesvisited[a + " "+ y] = True
+                pathsvisited[a + " "+ y] = True
                 break
-        #ebresi kai kataxorisi monopatiou
+                
+        # Search and save the path
         if (visited[start] == True and visited[end] == True): #prosthetoyme sth lista mia diadromi
-            diadromes.append(list(SVis))
-            boithvisited[end] = False
+            paths.append(list(SVis))
+            supvisited[end] = False
             b = SVis.pop(len(SVis) - 1)
             if (len(SVis) == 0):
                 telos = True
 
-        #ekserevnisi an exoume paei apo to start sto end me olous tous efiktous tropous
+        # Search if we have go from start to end with all available options
         listakleidiavis = list(visited.keys())
         episkepsi = 0
         for x in range(len(listakleidiavis)):
@@ -100,8 +114,8 @@ def calculate_dist():
     global Mikos
     Mikos = []
 
-    for i in range(len(diadromes)):
-        lista = list(diadromes[i])
+    for i in range(len(paths)):
+        lista = list(paths[i])
         sum = 0
         for j in range(len(lista) - 1):
             a = lista[j]
@@ -115,20 +129,25 @@ def calculate_bdist(start, end, bias):
     monopatia = []
     diadromesvoith = []
     efikta = {}
-    diadromesvoith = copy.deepcopy(diadromes)
-    for i in range(len(diadromes)):
+    diadromesvoith = copy.deepcopy(paths)
+
+    for i in range(len(paths)):
         efikta [i] = True
         monopatia.append(int(0))
+
     telos = False
     komvos = start
     Stackdiadromi.append(komvos)
-    #anazitisi monopation me merolipsia
+
+    # Searching paths with unfairness
     while (Stackdiadromi[len(Stackdiadromi) - 1] != end):
-        #ipologismos monopation me meropsia
+        
+        # Paths calculation with unfairness
         for k in range(len(diadromesvoith)):
             if (efikta[k] == True):
                 lista = list(diadromesvoith[k])
                 sum = 0
+
                 for j in range(len(lista) - 1):
                     a = lista[j]
                     b = lista[j + 1]
@@ -138,22 +157,26 @@ def calculate_bdist(start, end, bias):
                         prosthesi = bias * int(distance.get(a + " " + b))
                     sum = sum + prosthesi
                 monopatia[k] = sum
-        #mikrotero monopati
+
+        #Smallest path
         m = min(monopatia)
         thesi = -1
         for i in range(len(monopatia)):
             if (monopatia[i] == m):
                 thesi = i
-        #metafora se epomeno komvo
+
+        #go to another node
         for i in range(len(diadromesvoith)):
             if (len(diadromesvoith[i]) > 0 and efikta.get(i) == True):
                 diadromesvoith[i].pop(0)
         epomeno = diadromesvoith[thesi][0]
+
         for i in range(len(diadromesvoith)):
             lista = list(diadromesvoith[i])
             if (lista[0] != epomeno):
                 efikta[i] = False
-        #periptosi an exei minei ena monopati
+
+        #Case if we have left one path
         count = 0
         thesief = - 1
         for i in range(len(efikta)):
@@ -164,14 +187,17 @@ def calculate_bdist(start, end, bias):
             break
 
         Stackdiadromi.append(epomeno)
-    #diad to kalitero monopati
-    diad = list(diadromes[thesief])
-    #thesief h thesi stis listes, tou monopatiou
+
+    #diad : the best path
+    diad = list(paths[thesief])
+
+    #thesief : the position of the best path, in lists
     return thesief
 
 #The base of my program
 def main():
-    #apaitoumena gia konsola
+
+    #requirements for console
     parser = argparse.ArgumentParser()
     parser.add_argument("fn", help = "input_file")
     parser.add_argument("b", type = float, help = "bias_parameter")
@@ -194,11 +220,11 @@ def main():
         if (Mikos[i] < m):
             m = Mikos[i]
             thesim = i
-    path_1 = list(diadromes[thesim])
+    path_1 = list(paths[thesim])
     kostos_1 = int(m)
 
     thesi_2 = calculate_bdist(start, end, bias)
-    path_2 = list(diadromes[thesi_2])
+    path_2 = list(paths[thesi_2])
     kostos_2 = Mikos[thesi_2]
 
     msg_1 = str(path_1) + " " + str(kostos_1)
